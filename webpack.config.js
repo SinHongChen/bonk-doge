d43d1c72d0f87f8dc5ac9950bdfa2cf211540398
Jack <jack2316006@gmail.com>
2021-12-25 22:00:09 +0800

    [!] Fixed webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
 const path = require("path");
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     mode: "production",
+    target: "node",
     entry: "./src/index.js",
     output: {
         filename: "index.bundle.js",
-        path: path.resolve(__dirname, "./dist"),
+        path: path.resolve(__dirname, "dist"),
     },
+    externals: [
+        nodeExternals()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(graphql|gql)$/,
+                exclude: /node_modules/,
+                loader: '@graphql-tools/webpack-loader'
+            }
+        ]
+    },
+    optimization: {
+        chunkIds: "size",
+        // method of generating ids for chunks
+        moduleIds: "size",
+        // method of generating ids for modules
+        mangleExports: "size",
+        // rename export names to shorter names
+        minimize: true
+    }
 };
